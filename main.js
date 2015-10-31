9b53dc549cd10eaa80ba06e072a09d4720366800
Damon Kalaboukis <damonkala@yahoo.com>
2015-10-30 23:22:19 -0700

    Fully working game

--- main.js
+++ main.js
@@ -6,18 +6,37 @@ function init() {
 	console.log('ready!');
 	$('.block').on('click', chooseBlock);
 	$('.container').on('click', selectContainer)
+	var winner = false;
 }
 
 function chooseBlock () {
 	event.stopPropagation();
-	if ($(this).hasClass('selected')) {
-				$('.block').removeClass('selected')
-			} else {
-				$('.block').removeClass('selected')
-				$(this).addClass('selected');
-			}
+	console.log($('.selected').data('size'));
+	if ($(this).hasClass('selected')){
+		$(this).removeClass('selected')
+	}
+	if ($(this).is(':first-child')){
+		$('.block').removeClass('selected');
+		$(this).addClass('selected');
+	}
+// if(data === 1){
+// 	$('#one').addClass('selected');
+// 	console.log($('#container1').data('size'));
+// }
 }
 
 function selectContainer(event) {
-	 $('.selected').appendTo(this);
+
+	var put = $('.selected').data('size');
+	var next = $(this).find(':first-child').data("size");
+
+	if (next === undefined || next > put){
+		$('.selected').prependTo(this);
+		console.log(this.childNodes.length);
+	}
+if ($('#container3')[0].childNodes.length === 3) {
+	console.log("You're Winner");
+
 }
+
+	}
